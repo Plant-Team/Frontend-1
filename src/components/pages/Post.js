ab8253502810e2a5ecc5b8f130d8b45fd7f4d94f
Your Name <you@example.com>
2022-08-05 13:56:38 -0700

    fetched data

--- src/components/pages/Post.js
+++ src/components/pages/Post.js
@@ -1,9 +1,40 @@
+import { useEffect, useState } from "react";
+// import axios from 'axios'
+
 const Post = () => {
-    return (
-      <>
-      <h1>This Post</h1>
-      </>
-    );
+  const [posts, setPosts] = useState([]);
+  const url = {
+    api: "https://secret-refuge-99565.herokuapp.com/api",
+    endpoint: "/plants",
   };
-  
-  export default Post;
+
+  useEffect(() => {
+    const url_api = `${url.api}${url.endpoint}`;
+    fetch(url_api)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+      });
+  }, []);
+
+  return (
+    <>
+   <div>
+    {posts.map((post,index) => {
+      return (
+        <div key={index}>
+
+          <img src={'https://i.imgur.com/1DZOV9D.jpeg'} alt={post.name}/>
+          <p>{post.name}</p>
+        </div>
+
+      )
+    })}
+   </div>
+
+    </>
+  );
+};
+
+export default Post;
