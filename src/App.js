87806c8ace82ed30bff5b5596df58634ee0c7a70
Your Name <you@example.com>
2022-08-05 10:31:52 -0700

    compo added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import Landing from "./components/pages/Landing";
 import SignUp from "./components/pages/SignUp";
 import Navigation from "./components/navbar/Navigation";
 import Home from "./components/pages/Home";
-import Saved from "./components/pages/Saved";
+// import Saved from "./components/pages/Saved";
+import About from "./components/pages/About";
+import MyPosts from "./components/pages/MyPosts";
 
 function App() {
 
@@ -29,7 +31,9 @@ function App() {
           <Route path="/" element={<Landing />} />
           <Route path="/SignUp" element={<SignUp />} />
           <Route path="/home" element={<Home/>} />
-          <Route path="/saved" element={<Saved/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/myposts" element={<MyPosts/>} />
+          {/* <Route path="/saved" element={<Saved/>} /> */}
         </Routes>
       </main>
     </>
