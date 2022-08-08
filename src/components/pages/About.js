26da3efe9847feafae3d606e7b254aadfe577b8e
Csbw0319 <cwilkins0319@icloud.com>
2022-08-08 17:10:06 -0400

    added to about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,7 +6,7 @@ const About = () => {
        <h1 className='about-title'> About Plant World </h1>
        <h3 className='about'>WHAT WE DO</h3>
        <p className='about-text'>Welcome to plant world! We are a trading platform that allows users to trade their plants with other users. Our site mission is to allievate costs of plants for anyone that enjoy's planting.</p>
-       <img src='' alt='' />
+       <img className='about-image' src='https://images.pexels.com/photos/807598/pexels-photo-807598.jpeg?cs=srgb&dl=pexels-sohail-nachiti-807598.jpg&fm=jpg' alt='' />
     </div>
     
   )
