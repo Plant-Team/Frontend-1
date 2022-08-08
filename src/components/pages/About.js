0da4a058878480b92c8c2be672f30c01b985e694
Csbw0319 <cwilkins0319@icloud.com>
2022-08-08 13:39:37 -0400

    added some text to about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <div>
        <h1> About Plant World </h1>
        <h3>WHAT WE DO</h3>
-       <p>Welcome to plant world! We are a trading platform that allows users to trade their plants with </p>
+       <p>Welcome to plant world! We are a trading platform that allows users to trade their plants with other users. Our site mission is to allievate costs of plants for families that enjoy planting.</p>
        <img src='' alt='' />
     </div>
     
