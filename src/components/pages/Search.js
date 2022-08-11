5da1dcf8d57c3da6b5a5376e8a44e120e61e68a2
Your Name <you@example.com>
2022-08-10 19:17:41 -0700

    fixed formatting

--- src/components/pages/Search.js
+++ src/components/pages/Search.js
@@ -1,22 +1,19 @@
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Search = ({ handleChange, handleSearch }) => {
   return (
     <>
-    <div className="search-wrapper">
-
-      <form className="search-form" onSubmit={handleSearch}>
-        <input className='search-input' onChange={handleChange} type="text" />
-      
-      
-        <button className='search-button' type="submit">Search</button>
-        <Link to='/createpost'><button className='add-post'>add post</button></Link>
-      </form>
-      
-
-      
-     
-    </div>
+      <div className="search-wrapper">
+        <form className="search-form" onSubmit={handleSearch}>
+          <input className="search-input" onChange={handleChange} type="text" />
+          <button className="search-button" type="submit">
+            Search
+          </button>
+          <Link to="/createpost">
+            <button className="add-post">add post</button>
+          </Link>
+        </form>
+      </div>
     </>
   );
 };
